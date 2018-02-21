10d23ee6c852d33f74adbbf270ab5d7e35c91706
ladislavborbely <ladislavborbely@gmail.com>
2018-02-21 20:13:37 +0100

    Initial commit

---
+++ src/main/java/eu/haktes/trav/projectA/App.java
@@ -0,0 +1,19 @@
+package eu.haktes.trav.projectA;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {    	
+    	System.setProperty("webdriver.chrome.driver", "C:\\Java\\chromedriver.exe");
+    	WebDriver driver = new ChromeDriver();
+        System.out.println( "Hello World!" );
+        driver.get("https://google.com");
+    }
+}
---
+++ src/main/java/eu/haktes/trav/projectA/package-info.java
@@ -0,0 +1,7 @@
+package eu.haktes.trav.projectA;
+
+//treba mat stiahnutu 1.8 JDK
+
+//nastavit pre eclipse https://stackoverflow.com/questions/19655184/no-compiler-is-provided-in-this-environment-perhaps-you-are-running-on-a-jre-ra
+
+//nastavit aj pre projekt ked to hubuje

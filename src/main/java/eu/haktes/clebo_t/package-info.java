239c8ecc32ac3e359155434ed8f53bb09f202c61
Haktes <haktes@gmail.com>
2018-02-21 20:41:52 +0100

    instructions added

--- src/main/java/eu/haktes/clebo_t/package-info.java
+++ src/main/java/eu/haktes/clebo_t/package-info.java
@@ -1,7 +1,13 @@
 package eu.haktes.clebo_t;
 
-//treba mat stiahnutu 1.8 JDK
+// treba mat stiahnutu 1.8 JDK
+// eclipse ak nezoberie maven tak treba stiahnut aktulanu, nastavit a pridat do
+// eclipsu vid net
 
-//nastavit pre eclipse https://stackoverflow.com/questions/19655184/no-compiler-is-provided-in-this-environment-perhaps-you-are-running-on-a-jre-ra
+// treba mat selenium driver pre chrome (je to exe subor) a dat ho na spravnu -
+// nami zvolenu cestu (C:\Java\chromedriver.exe)
 
-//nastavit aj pre projekt ked to hubuje
+// nastavit pre eclipse java 1.8
+// https://stackoverflow.com/questions/19655184/no-compiler-is-provided-in-this-environment-perhaps-you-are-running-on-a-jre-ra
+
+// nastavit aj pre projekt 1.8 ked to hubuje

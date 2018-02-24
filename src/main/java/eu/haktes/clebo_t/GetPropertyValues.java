aadc2b5f6a28abed47971a0c15c07ace2257c817
Haktes <haktes@gmail.com>
2018-02-24 13:30:38 +0100

    mix - POM upd, jar generation

---
+++ src/main/java/eu/haktes/clebo_t/GetPropertyValues.java
@@ -0,0 +1,48 @@
+package eu.haktes.clebo_t;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Properties;
+ 
+/**
+ * https://crunchify.com/java-properties-file-how-to-read-config-properties-values-in-java/
+ * 
+ */
+ 
+public class GetPropertyValues {
+	String result = "";
+	InputStream inputStream;
+ 
+	public Properties getPropValues() throws IOException {
+		Properties prop = new Properties();
+		try {
+			
+			String propFileName = "application.properties";
+ 
+			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+ 
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+ 
+			Date time = new Date(System.currentTimeMillis());
+ 
+			// get the property value and print it out
+//			String user = prop.getProperty("user");
+//			String company1 = prop.getProperty("company1");
+//			String company2 = prop.getProperty("company2");
+//			String company3 = prop.getProperty("company3");
+// 
+//			result = "Company List = " + company1 + ", " + company2 + ", " + company3;
+//			System.out.println(result + "\nProgram Ran on " + time + " by user=" + user);
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			inputStream.close();
+		}
+		return prop;
+	}
+}
--- src/main/java/eu/haktes/clebo_t/package-info.java
+++ src/main/java/eu/haktes/clebo_t/package-info.java
@@ -11,3 +11,18 @@ package eu.haktes.clebo_t;
 // https://stackoverflow.com/questions/19655184/no-compiler-is-provided-in-this-environment-perhaps-you-are-running-on-a-jre-ra
 
 // nastavit aj pre projekt 1.8 ked to hubuje
+
+// trav. strategy guide http://www.travian-strategy-guide.com/advanced-techniques/village-specialization
+
+/*
+//Locate element for which you wants to retrieve x y coordinates.
+WebElement Image = driver.findElement(By.xpath("//img[@border='0']"));
+
+//Used points class to get x and y coordinates of element.
+Point point = Image.getLocation();
+int xcord = point.getX();
+System.out.println("Element's Position from left side Is "+xcord +" pixels.");
+int ycord = point.getY();
+System.out.println("Element's Position from top side Is "+ycord +" pixels.");
+
+*/

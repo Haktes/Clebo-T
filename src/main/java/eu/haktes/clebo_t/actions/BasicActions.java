016125e0ef4b83672b9b7584e359fd49f3fcf9a7
vesely5 <dzavatar.peto@gmail.com>
2018-03-27 16:59:23 +0200

    update basicActions.java

--- src/main/java/eu/haktes/clebo_t/actions/BasicActions.java
+++ src/main/java/eu/haktes/clebo_t/actions/BasicActions.java
@@ -12,147 +12,7 @@ import eu.haktes.clebo_t.utils.BasicUtils;
 
 public class BasicActions {
 
-<<<<<<< HEAD
-    private static final Logger LOG =
-        (Logger) LogManager.getLogger(BasicActions.class);
 
-    static WebDriver driver;
-
-    public BasicActions(WebDriver passDriver) {
-
-        driver = passDriver;
-    }
-
-    /**
-     * Login
-     */
-    public void loginUser(String name, String password, String link) {
-
-        driver.get(link);
-        driver.findElement(By.xpath("//input[@name='name']")).sendKeys(name);
-        driver.findElement(By.xpath("//input[@name='password']"))
-            .sendKeys(password);
-        driver.findElement(By.xpath("//input[@name='password']"))
-            .sendKeys(Keys.ENTER);
-    }
-
-    /**
-     * Get resources in current village or null if cant
-     */
-    public Resources getResources(Resources resources) {
-        // By.xpath("//div[@class='alert alert-count']/p/b)
-
-        try {
-            WebElement weResourcePanel =
-                driver.findElement(By.xpath("//ul[@id='stockBar']"));
-
-            String max = weResourcePanel
-                .findElement(By.xpath("//span[@id='stockBarWarehouse']"))
-                .getText();
-            String maxCrop = weResourcePanel
-                .findElement(By.xpath("//span[@id='stockBarGranary']"))
-                .getText();
-            String res1 = weResourcePanel
-                .findElement(By.xpath("//li[@id='stockBarResource1']"))
-                .getText();
-            String res2 = weResourcePanel
-                .findElement(By.xpath("//li[@id='stockBarResource2']"))
-                .getText();
-            String res3 = weResourcePanel
-                .findElement(By.xpath("//li[@id='stockBarResource3']"))
-                .getText();
-            String res4 = weResourcePanel
-                .findElement(By.xpath("//li[@id='stockBarResource4']"))
-                .getText();
-
-            resources.setMaximum(BasicUtils.stringToInt(max));
-            resources.setMaximumCrop(BasicUtils.stringToInt(maxCrop));
-            resources.setWood(BasicUtils.stringToInt(res1));
-            resources.setClay(BasicUtils.stringToInt(res2));
-            resources.setIron(BasicUtils.stringToInt(res3));
-            resources.setCrop(BasicUtils.stringToInt(res4));
-
-            return resources;
-
-        } catch (Exception e) {
-            LOG.error("Can't get resources!");
-        }
-
-        return null;
-    }
-    
-    public void getHeroStatus(HeroStatus hero) {
-    	
-    	WebElement weHeroStats = driver.findElement(By.xpath("//*[@id=\"heroImageButton\"]"));
-		//moveMouse.moveMouse(lastMousePos, pathGenerator.getMovementArray(lastMousePos, weCentrumDediny));
-		weHeroStats.click();
-    	
-    	////*[@id="attributes"]/div[1]/div[3]/table/tbody/tr[1]/td[2]/span
-    	WebElement heroStats =
-                driver.findElement(By.xpath("//*[@id=\"content\"]"));
-    	
-    	String health = heroStats
-                .findElement(By.xpath("//*[@id=\"attributes\"]/div[1]/div[3]/table/tbody/tr[1]/td[2]/span"))
-                .getText();
-    	System.out.println(health);
-    	health=health.replace("%", "");
-    	System.out.println(health);
-    	int healthInt=Integer.parseInt(health);
-    	System.out.println(healthInt);
-    	hero.setHealth(healthInt);
-    	
-    	String experiences = heroStats
-                .findElement(By.xpath("//*[@id=\"attributes\"]/div[1]/div[3]/table/tbody/tr[2]/td[2]/span"))
-                .getText();
-    	int experiencesInt=Integer.parseInt(experiences);
-    	hero.setXp(experiencesInt);
-    	
-    	String speedPerField = heroStats
-                .findElement(By.xpath("//*[@id=\"heroSpeedValueNumber\"]"))
-                .getText();
-    	int speedPerFieldInt=Integer.parseInt(speedPerField);
-    	hero.setSpeedPerField(speedPerFieldInt);
-    	
-    	String plusProduction = heroStats
-                .findElement(By.xpath("//*[@id=\"attributes\"]/div[1]/div[3]/table/tbody/tr[3]/td[2]/span/span[1]/span"))
-                .getText();
-    	int plusProductionInt=Integer.parseInt(plusProduction);
-    	hero.setSpeedPerField(plusProductionInt);
-    	
-    	Boolean heroHidden = heroStats
-                .findElement(By.xpath("//*[@id=\"attackBehaviourHide\"]"))
-                .isSelected();
-    	hero.setHeroHidden(heroHidden);
-    	
-    	String strength = heroStats
-                .findElement(By.xpath("//*[@id=\"attributepower\"]/td[2]/span"))
-                .getText();
-    	int strengthInt=Integer.parseInt(strength);
-    	hero.setStrength(strengthInt);
-    	
-    	String offBonus = heroStats
-                .findElement(By.xpath("//*[@id=\"attributeoffBonus\"]/td[2]/span"))
-                .getText();
-    	offBonus=offBonus.replace("%", "");
-    	int offBonusInt=Integer.parseInt(offBonus);
-    	hero.setOffBonus(offBonusInt);
-    	
-    	String deffBonus = heroStats
-                .findElement(By.xpath("//*[@id=\"attributedefBonus\"]/td[2]/span"))
-                .getText();
-    	deffBonus=deffBonus.replace("%", "");
-    	int deffBonusInt=Integer.parseInt(deffBonus);
-    	hero.setDeffBonus(deffBonusInt);
-    	
-    	String productionPoints = heroStats
-                .findElement(By.xpath("//*[@id=\"attributeproductionPoints\"]/td[2]/span"))
-                .getText();
-    	int productionPointsInt=Integer.parseInt(productionPoints);
-    	hero.setProductionPoints(productionPointsInt);
-    	
-    	
-    }
-=======
 	private static final Logger LOG = (Logger) LogManager.getLogger(BasicActions.class);
 
 	static WebDriver driver;
@@ -226,6 +86,78 @@ public class BasicActions {
 
 		return null;
 	}
->>>>>>> 5c037990ad330b069c8b1f7e12030b9f8d088d1d
+	
+public void getHeroStatus(HeroStatus hero) {
+    	
+    	WebElement weHeroStats = driver.findElement(By.xpath("//*[@id=\"heroImageButton\"]"));
+		//moveMouse.moveMouse(lastMousePos, pathGenerator.getMovementArray(lastMousePos, weCentrumDediny));
+		weHeroStats.click();
+    	
+    	////*[@id="attributes"]/div[1]/div[3]/table/tbody/tr[1]/td[2]/span
+    	WebElement heroStats =
+                driver.findElement(By.xpath("//*[@id=\"content\"]"));
+    	
+    	String health = heroStats
+                .findElement(By.xpath("//*[@id=\"attributes\"]/div[1]/div[3]/table/tbody/tr[1]/td[2]/span"))
+                .getText();
+    	System.out.println(health);
+    	health=health.replace("%", "");
+    	System.out.println(health);
+    	int healthInt=Integer.parseInt(health);
+    	System.out.println(healthInt);
+    	hero.setHealth(healthInt);
+    	
+    	String experiences = heroStats
+                .findElement(By.xpath("//*[@id=\"attributes\"]/div[1]/div[3]/table/tbody/tr[2]/td[2]/span"))
+                .getText();
+    	int experiencesInt=Integer.parseInt(experiences);
+    	hero.setXp(experiencesInt);
+    	
+    	String speedPerField = heroStats
+                .findElement(By.xpath("//*[@id=\"heroSpeedValueNumber\"]"))
+                .getText();
+    	int speedPerFieldInt=Integer.parseInt(speedPerField);
+    	hero.setSpeedPerField(speedPerFieldInt);
+    	
+    	String plusProduction = heroStats
+                .findElement(By.xpath("//*[@id=\"attributes\"]/div[1]/div[3]/table/tbody/tr[3]/td[2]/span/span[1]/span"))
+                .getText();
+    	int plusProductionInt=Integer.parseInt(plusProduction);
+    	hero.setSpeedPerField(plusProductionInt);
+    	
+    	Boolean heroHidden = heroStats
+                .findElement(By.xpath("//*[@id=\"attackBehaviourHide\"]"))
+                .isSelected();
+    	hero.setHeroHidden(heroHidden);
+    	
+    	String strength = heroStats
+                .findElement(By.xpath("//*[@id=\"attributepower\"]/td[2]/span"))
+                .getText();
+    	int strengthInt=Integer.parseInt(strength);
+    	hero.setStrength(strengthInt);
+    	
+    	String offBonus = heroStats
+                .findElement(By.xpath("//*[@id=\"attributeoffBonus\"]/td[2]/span"))
+                .getText();
+    	offBonus=offBonus.replace("%", "");
+    	int offBonusInt=Integer.parseInt(offBonus);
+    	hero.setOffBonus(offBonusInt);
+    	
+    	String deffBonus = heroStats
+                .findElement(By.xpath("//*[@id=\"attributedefBonus\"]/td[2]/span"))
+                .getText();
+    	deffBonus=deffBonus.replace("%", "");
+    	int deffBonusInt=Integer.parseInt(deffBonus);
+    	hero.setDeffBonus(deffBonusInt);
+    	
+    	String productionPoints = heroStats
+                .findElement(By.xpath("//*[@id=\"attributeproductionPoints\"]/td[2]/span"))
+                .getText();
+    	int productionPointsInt=Integer.parseInt(productionPoints);
+    	hero.setProductionPoints(productionPointsInt);
+    	
+    	
+    }
+
 
 }

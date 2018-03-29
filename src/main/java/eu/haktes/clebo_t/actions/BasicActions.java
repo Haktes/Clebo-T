445504fa740aa05724c60db8d42f6ee4dba8e0aa
vesely5 <dzavatar.peto@gmail.com>
2018-03-29 15:59:54 +0200

    getHeroStatus method - working

--- src/main/java/eu/haktes/clebo_t/actions/BasicActions.java
+++ src/main/java/eu/haktes/clebo_t/actions/BasicActions.java
@@ -103,26 +103,26 @@ public void getHeroStatus(HeroStatus hero) {
     	System.out.println(health);
     	health=health.replace("%", "");
     	System.out.println(health);
-    	int healthInt=Integer.parseInt(health);
+    	int healthInt=BasicUtils.stringToInt(health);
     	System.out.println(healthInt);
     	hero.setHealth(healthInt);
     	
     	String experiences = heroStats
                 .findElement(By.xpath("//*[@id=\"attributes\"]/div[1]/div[3]/table/tbody/tr[2]/td[2]/span"))
                 .getText();
-    	int experiencesInt=Integer.parseInt(experiences);
+    	int experiencesInt=BasicUtils.stringToInt(experiences);
     	hero.setXp(experiencesInt);
     	
     	String speedPerField = heroStats
                 .findElement(By.xpath("//*[@id=\"heroSpeedValueNumber\"]"))
                 .getText();
-    	int speedPerFieldInt=Integer.parseInt(speedPerField);
+    	int speedPerFieldInt=BasicUtils.stringToInt(speedPerField);
     	hero.setSpeedPerField(speedPerFieldInt);
     	
     	String plusProduction = heroStats
                 .findElement(By.xpath("//*[@id=\"attributes\"]/div[1]/div[3]/table/tbody/tr[3]/td[2]/span/span[1]/span"))
                 .getText();
-    	int plusProductionInt=Integer.parseInt(plusProduction);
+    	int plusProductionInt=BasicUtils.stringToInt(plusProduction);
     	hero.setSpeedPerField(plusProductionInt);
     	
     	Boolean heroHidden = heroStats
@@ -133,27 +133,27 @@ public void getHeroStatus(HeroStatus hero) {
     	String strength = heroStats
                 .findElement(By.xpath("//*[@id=\"attributepower\"]/td[2]/span"))
                 .getText();
-    	int strengthInt=Integer.parseInt(strength);
+    	int strengthInt=BasicUtils.stringToInt(strength);
     	hero.setStrength(strengthInt);
     	
     	String offBonus = heroStats
                 .findElement(By.xpath("//*[@id=\"attributeoffBonus\"]/td[2]/span"))
                 .getText();
     	offBonus=offBonus.replace("%", "");
-    	int offBonusInt=Integer.parseInt(offBonus);
+    	int offBonusInt=BasicUtils.stringToInt(offBonus);
     	hero.setOffBonus(offBonusInt);
     	
     	String deffBonus = heroStats
                 .findElement(By.xpath("//*[@id=\"attributedefBonus\"]/td[2]/span"))
                 .getText();
     	deffBonus=deffBonus.replace("%", "");
-    	int deffBonusInt=Integer.parseInt(deffBonus);
+    	int deffBonusInt=BasicUtils.stringToInt(deffBonus);
     	hero.setDeffBonus(deffBonusInt);
     	
     	String productionPoints = heroStats
                 .findElement(By.xpath("//*[@id=\"attributeproductionPoints\"]/td[2]/span"))
                 .getText();
-    	int productionPointsInt=Integer.parseInt(productionPoints);
+    	int productionPointsInt=BasicUtils.stringToInt(productionPoints);
     	hero.setProductionPoints(productionPointsInt);
     	
     	
